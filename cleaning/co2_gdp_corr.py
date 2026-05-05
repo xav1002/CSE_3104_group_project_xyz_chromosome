137355114b58d45349c82146d69c591e625efe0e
xav1002 <v.a.xu@wustl.edu>
2026-05-05 13:28:15 -0500

    generalized

--- cleaning/co2_gdp_corr.py
+++ cleaning/co2_gdp_corr.py
@@ -2,40 +2,41 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-gdp = pd.read_csv("CSE_3104_group_project_xyz_chromosome/data/gdp_pcap.csv")
-co2 = pd.read_csv("CSE_3104_group_project_xyz_chromosome/data/co2_pcap_cons.csv")
-gdp_long = gdp.melt(
-    id_vars=["geo","name"],
-    var_name="year",
-    value_name="gdp_per_capita"
-)
-co2_long = co2.melt(
-    id_vars=["geo","name"],
-    var_name="year",
-    value_name="co2_per_capita"
-)
-gdp_long["year"] = gdp_long["year"].astype(int)
-co2_long["year"] = co2_long["year"].astype(int)
-gdp_long = gdp_long[(gdp_long["year"] >= 1960) & (gdp_long["year"] <= 2022)]
-co2_long = co2_long[(co2_long["year"] >= 1960) & (co2_long["year"] <= 2022)]
-df = gdp_long.merge(
-    co2_long,
-    on=["geo","name","year"],
-    how="inner"
-)
-df.isna().sum()
-df = df.dropna()
+def run():
+    gdp = pd.read_csv("data/gdp_pcap.csv")
+    co2 = pd.read_csv("data/co2_pcap_cons.csv")
+    gdp_long = gdp.melt(
+        id_vars=["geo","name"],
+        var_name="year",
+        value_name="gdp_per_capita"
+    )
+    co2_long = co2.melt(
+        id_vars=["geo","name"],
+        var_name="year",
+        value_name="co2_per_capita"
+    )
+    gdp_long["year"] = gdp_long["year"].astype(int)
+    co2_long["year"] = co2_long["year"].astype(int)
+    gdp_long = gdp_long[(gdp_long["year"] >= 1960) & (gdp_long["year"] <= 2022)]
+    co2_long = co2_long[(co2_long["year"] >= 1960) & (co2_long["year"] <= 2022)]
+    df = gdp_long.merge(
+        co2_long,
+        on=["geo","name","year"],
+        how="inner"
+    )
+    df.isna().sum()
+    df = df.dropna()
 
-df["log_gdp"] = np.log(df["gdp_per_capita"])
-df["co2_lag1"] = df.groupby("geo")["co2_per_capita"].shift(1)
-df[["gdp_per_capita","co2_per_capita"]].corr()
+    df["log_gdp"] = np.log(df["gdp_per_capita"])
+    df["co2_lag1"] = df.groupby("geo")["co2_per_capita"].shift(1)
+    df[["gdp_per_capita","co2_per_capita"]].corr()
 
-print(df.head())
+    # print(df.head())
 
-plt.scatter(df["co2_per_capita"], df["gdp_per_capita"])
+    plt.scatter(df["co2_per_capita"], df["gdp_per_capita"])
 
-plt.xlabel("CO2 per capita")
-plt.ylabel("GDP per capita")
-plt.title("CO2 Emissions vs GDP per Capita")
+    plt.xlabel("CO2 per capita")
+    plt.ylabel("GDP per capita")
+    plt.title("CO2 Emissions vs GDP per Capita")
 
-plt.show()
+    plt.show()
